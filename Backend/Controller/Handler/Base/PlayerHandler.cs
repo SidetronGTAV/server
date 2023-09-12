f38fdb0db58f34fbf16277bc5c4150eb0ce1214d
Nico Ullrich <Letscrafter@outlook.de>
2023-09-13 00:47:21 +0200

    Add DateTime Now

--- Backend/Controller/Handler/Base/PlayerHandler.cs
+++ Backend/Controller/Handler/Base/PlayerHandler.cs
@@ -18,6 +18,7 @@ public static class PlayerHandler
         player.Position = new Position(-1562.5055f, -579.6528f, 108.50769f);
         player.Model = (uint)PedModel.FreemodeMale01;
         player.Frozen = true;
+        player.SetDateTime(DateTime.Now);
     }
     
     public static void HandlePlayerDisconnect(MyPlayer player)
