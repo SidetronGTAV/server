8a186ed98de357a98611f3e24ed109a48ded4784
Letsion <Letscrafter@outlook.de>
2024-01-19 23:42:44 +0100

    Colshape Voice Change implemented

--- Frontend/client/src/base/Voice.ts
+++ Frontend/client/src/base/Voice.ts
@@ -5,13 +5,18 @@ import { Hud } from './Handling/Hud.js';
 
 export class Voice {
      private static _voiceLevel: number = 1;
+     private static _timeout: number | null = null;
+     private static _marker: alt.Marker | null = null;
+     private static readonly markerColor: alt.RGBA = new alt.RGBA(255, 255,255, 30);
+
 
      constructor() {
           alt.on(Events.alt.keyup, Voice.onKeyUp);
-          alt.onServer(Events.Voice.UpdateMicrophoneLevel, Hud.changeMicrophone);
+          alt.onServer(Events.Voice.UpdateMicrophoneLevel, Voice.changeHudVoiceLevel);
+          alt.everyTick(Voice.everyTick);
      }
 
-     public static onKeyUp(key: KeyCode) {
+     private static onKeyUp(key: KeyCode) {
           switch (key) {
                case KeyCode['.']:
                     Voice._voiceLevel++;
@@ -23,5 +28,52 @@ export class Voice {
                     alt.emitServer(Events.Voice.Toggle, Voice._voiceLevel);
                     break;
           }
+     }
+
+     private static seeColshape() {
+          let radius: number;
+          switch (Voice._voiceLevel) {
+               case 1:
+                    radius = 2.6;
+                    break;
+               case 2:
+                    radius = 10;
+                    break;
+               case 3:
+                    radius = 25;
+                    break;
+          }
+
+          Voice.destroyColshape();
+          const localPlayer = alt.Player.local.pos;
+          Voice._marker = new alt.Marker(alt.MarkerType.MarkerHalo, new alt.Vector3(localPlayer.x, localPlayer.y, localPlayer.z - 0.98), Voice.markerColor,false);
+          Voice._marker.scale = new alt.Vector3(radius);
+          Voice._marker.dimension = alt.Player.local.dimension;
+          Voice._marker.visible = true;
+
+          Voice._timeout = alt.setTimeout(() => {
+               Voice._marker.destroy();
+               Voice._timeout = null;
+               Voice._marker = null;
+          }, 370);
+     }
+
+     private static destroyColshape() {
+          if (Voice._timeout) alt.clearTimeout(Voice._timeout);
+          if (Voice._marker) Voice._marker.destroy();
+          Voice._marker = null;
+          Voice._timeout = null;
+     }
+
+     private static everyTick() {
+          if (Voice._marker) {
+               const localPlayer = alt.Player.local.pos;
+               Voice._marker.pos = new alt.Vector3(localPlayer.x, localPlayer.y, localPlayer.z - 0.98);
+          }
+     }
+
+     private static changeHudVoiceLevel(voiceLevel: number) {
+          Hud.changeMicrophone(voiceLevel);
+          Voice.seeColshape();
      }
 }
